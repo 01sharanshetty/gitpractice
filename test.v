21d04dade8156306eb58fc0c64edc6ccb5f9c2cb
01sharanshetty <126483491+01sharanshetty@users.noreply.github.com>
2023-06-12 11:55:23 +0530

    Update test.v

--- test.v
+++ test.v
@@ -1,8 +1,8 @@
-module adder_a(a,b,c,sum,cout)
+module adder_a(a,b,c,sum,cout);
 input a,b,c;
 output sum,cout;
-wire sum,cout
+
 assign sum=a^b^c;
-assign cout=a&b|b&c|c&a
+assign cout=a&b|b&c|c&a;
 endmodule
 
